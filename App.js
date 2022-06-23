9ce86ba3d8d16d2688cc06f2a4b30b2232bb6211
Sadique <mhsadique1307@gmail.com>
2022-06-23 22:36:43 +0600

    almost done with folder and file structure

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { spacing } from './src/theme/spacing';
+import { useFonts } from 'expo-font';
+import { typography } from './src/theme/typography';
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/LeagueSpartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/LeagueSpartan-Regular.ttf')
+  });
+
+  if (!loaded) {
+    return <Text>Font is loading...</Text>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={{
+        marginTop: spacing[8],
+        fontFamily: typography.primary,
+        fontSize: 30
+      }}>
+        Open up App.js to start
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
