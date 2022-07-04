9ffb96e34575c37397f13a32af4c8c7235ccae4c
Sadique <mhsadique1307@gmail.com>
2022-07-04 23:22:38 +0600

    details screen done

--- src/screens/details.js
+++ src/screens/details.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet, Pressable } from 'react-native';
+import { View, ScrollView, StyleSheet, Pressable, Linking } from 'react-native';
 import Text from '../components/text/text';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import PlanetHeader from '../components/planet-header';
@@ -8,6 +8,19 @@ import { PLANET_LIST } from '../data/planet-data'
 import { spacing } from '../theme/spacing';
 import { EarthSvg, JupiterSvg, MarsSvg, MercurySvg, NeptuneSvg, SaturnSvg, UranusSvg, VenusSvg } from '../svg/index';
 import { EvilIcons } from '@expo/vector-icons';
+
+const PlanetSection = ({ title, value }) => {
+    return (
+        <View style={styles.planetSection}>
+            <Text preset='small' style={{ textTransform: 'uppercase' }}>
+                {title}
+            </Text>
+            <Text preset='h2'>
+                {value}
+            </Text>
+        </View>
+    )
+};
 
 const renderImage = (name) => {
     switch (name) {
@@ -31,9 +44,14 @@ const renderImage = (name) => {
 };
 
 const Details = ({ route }) => {
+
+    const onPressLink = (wikiLink) => {
+        Linking.openURL(wikiLink);
+    }
+
     const { planetIndex } = route.params;
     const planetData = PLANET_LIST.find((planet, index) => index === planetIndex);
-    const { name, color, description, rotationTime, radius, avgTemp, wikiLink, structure, surface } = planetData;
+    const { name, color, description, rotationTime, radius, avgTemp, wikiLink, structure, surface, revolutionTime } = planetData;
     return (
         <SafeAreaView style={styles.container}>
             <PlanetHeader backBtn={true} title={name.toUpperCase()} />
@@ -44,12 +62,19 @@ const Details = ({ route }) => {
                 <View style={styles.detailsView}>
                     <Text preset='h1' style={styles.planetName}>{name}</Text>
                     <Text style={styles.description}>{description}</Text>
-                    <Pressable style={styles.source}>
+                    <Pressable style={styles.source} onPress={() => onPressLink(wikiLink)} >
                         <Text>Source: </Text>
                         <Text style={styles.wikipedia}>Wikipedia</Text>
                         <EvilIcons name="external-link" size={24} color="white" />
                     </Pressable>
                 </View>
+
+                <View style={{ height: 40 }} />
+
+                <PlanetSection title="ROTATIONtIME" value={rotationTime} />
+                <PlanetSection title="REVOLUTION TIME" value={revolutionTime} />
+                <PlanetSection title="RADIUS" value={radius} />
+                <PlanetSection title="AVERAGE TEMP." value={avgTemp} />
             </ScrollView>
         </SafeAreaView>
     );
@@ -87,6 +112,17 @@ const styles = StyleSheet.create({
         textDecorationLine: 'underline',
         fontWeight: 'bold',
         marginTop: spacing[7]
+    },
+    planetSection: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: spacing[6],
+        paddingVertical: spacing[5],
+        borderWidth: 1,
+        borderColor: colors.grey,
+        marginHorizontal: spacing[7],
+        marginBottom: spacing[5]
     }
 })
 
